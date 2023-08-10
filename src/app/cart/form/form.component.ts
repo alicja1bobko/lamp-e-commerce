50bba0d74386c25a3673b8cd9054121129d824d6
Alicja Bobko <alicja.bobko@talan.com>
2023-08-10 10:48:06 +0200

    feat: form validation display errors

--- src/app/cart/form/form.component.ts
+++ src/app/cart/form/form.component.ts
@@ -8,28 +8,35 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 })
 export class FormComponent {
   contactForm!: FormGroup;
-
+  shippingMethod: string = 'inpost';
   constructor(private formBuilder: FormBuilder) {}
 
   ngOnInit(): void {
     this.contactForm = this.formBuilder.group({
       firstName: ['', [Validators.required, Validators.minLength(3)]],
       lastName: ['', [Validators.required, Validators.minLength(3)]],
-
       email: ['', [Validators.required, Validators.email]],
-      phone: ['', [Validators.required, Validators.minLength(3)]],
+      phone: ['', [Validators.required, Validators.minLength(10)]],
+      country: ['', [Validators.required]],
+      postalCode: ['', [Validators.required, Validators.minLength(5)]],
+      street: ['', [Validators.required, Validators.minLength(5)]],
+      note: ['', [Validators.minLength(3)]],
+      shippingMethod: ['', [Validators.required]],
+      payment: ['', [Validators.required]],
     });
   }
-
-  shippingMethod: string = '';
-  paymentMethod: string = '';
 
   submitForm() {
     const orderData = {
       contact: this.contactForm,
-      shippingMethod: this.shippingMethod,
-      paymentMethod: this.paymentMethod,
     };
+    this.contactForm.reset();
     console.log('Submitted Order:', orderData);
+  }
+
+  onShippingMethodChange(event: Event) {
+    const selectedShippingMethod = (event.target as HTMLInputElement).value;
+    this.shippingMethod = selectedShippingMethod;
+    console.log(this.shippingMethod);
   }
 }
--- src/app/cart/total-price/total-price.component.ts
+++ src/app/cart/total-price/total-price.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input, SimpleChanges } from '@angular/core';
 import { Item } from 'src/app/home/models/Item';
 import { CartService } from 'src/app/services/cart.service';
 
@@ -9,23 +9,48 @@ import { CartService } from 'src/app/services/cart.service';
 })
 export class TotalPriceComponent {
   cartItems$: Map<Item, number> = new Map<Item, number>();
-  total$: number = 0;
   subtotal$: number = 0;
-  shipping$: number = 20;
+  shipping$: number = 0;
+  total$: number = 0;
+
+  @Input() shippingMethod: string = 'inpost';
+
   constructor(private readonly cartService: CartService) {}
+
+  ngOnChanges(changes: SimpleChanges): void {
+    if ('shippingMethod' in changes) {
+      this.calculateShipping();
+      this.calculateTotal();
+    }
+  }
 
   ngOnInit(): void {
     this.cartService.getCartItems().subscribe((items) => {
       this.cartItems$ = items;
-      this.subtotal$ = this.calculateSubtotal();
-      this.total$ = this.subtotal$ + this.shipping$;
+      this.calculateSubtotal();
+      this.calculateTotal();
     });
   }
-  calculateSubtotal(): number {
-    let total = 0;
-    this.cartItems$.forEach((quantity, item) => {
-      total += item.price * quantity;
-    });
-    return total;
+
+  calculateSubtotal(): void {
+    this.subtotal$ = Array.from(this.cartItems$).reduce(
+      (total, [item, quantity]) => total + item.price * quantity,
+      0
+    );
+  }
+
+  calculateShipping(): void {
+    if (this.shippingMethod === 'pickup') {
+      this.shipping$ = 0;
+    } else if (this.shippingMethod === 'inpost') {
+      this.shipping$ = 20;
+    } else if (this.shippingMethod === 'DPD') {
+      this.shipping$ = 24;
+    }
+  }
+
+  calculateTotal(): void {
+    this.calculateShipping();
+    this.total$ = this.subtotal$ + this.shipping$;
   }
 }

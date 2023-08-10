741af3418de5950052337cfc777a86074a3eca32
Alicja Bobko <alicja.bobko@talan.com>
2023-08-10 14:15:40 +0200

    add more products

--- src/app/common components/navbar-light/navbar-light.component.ts
+++ src/app/common components/navbar-light/navbar-light.component.ts
@@ -1,10 +1,29 @@
 import { Component } from '@angular/core';
+import { Item } from 'src/app/home/models/Item';
+import { CartService } from 'src/app/services/cart.service';
 
 @Component({
   selector: 'app-navbar-light',
   templateUrl: './navbar-light.component.html',
-  styleUrls: ['./navbar-light.component.scss']
+  styleUrls: ['./navbar-light.component.scss'],
 })
 export class NavbarLightComponent {
+  cartItems$: Map<Item, number> = new Map<Item, number>();
+  total$: number = 0;
 
+  constructor(private readonly cartService: CartService) {}
+
+  ngOnInit(): void {
+    this.cartService.getCartItems().subscribe((items) => {
+      this.cartItems$ = items;
+      this.total$ = this.calculateTotalItems();
+    });
+  }
+  calculateTotalItems(): number {
+    let total = 0;
+    this.cartItems$.forEach((quantity, item) => {
+      total += quantity;
+    });
+    return total;
+  }
 }
